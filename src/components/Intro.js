9b2677c803af5da998c8a2f4be10f6c1967b5159
muskan2532150 <rg2532150@gmail.com>
2023-02-06 17:45:25 +0530

    Add Introuduction component

---
+++ src/components/Intro.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Button } from '@chakra-ui/react'
+import '../asserts/intro.css';
+
+const Intro = () => {
+  return (
+    <section>
+<h1>
+Discover the Best Food and Drinks
+</h1>
+<p>
+Naturally made Healthcare Products for the better care & support of your body.
+</p>
+<Button  
+size='md'
+  height='63px'
+  width='200px'
+  border='1px'
+  borderColor='#E23744'>Button</Button>
+    </section>
+  )
+}
+
+export default Intro
